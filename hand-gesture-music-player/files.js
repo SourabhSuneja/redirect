9ca3f8a1b931584fd568445c3f39aa6e3048e96b
SourabhSuneja <78584922+SourabhSuneja@users.noreply.github.com>
2025-03-06 11:32:15 +0530

    Create files.js

---
+++ hand-gesture-music-player/files.js
@@ -0,0 +1,14 @@
+let track_list = [
+  {
+    name: "Introduction to Excel",
+    artist: "By Sourabh Sir",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Microsoft_Excel_2013-2019_logo.svg/1200px-Microsoft_Excel_2013-2019_logo.svg.png",
+    path: "https://sourabhsuneja.github.io/academic-resources/class-6/audio/ch-5-excel-intro.mp3"
+  },
+  {
+    name: "How Excel Works?",
+    artist: "By Sourabh Sir",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Microsoft_Excel_2013-2019_logo.svg/1200px-Microsoft_Excel_2013-2019_logo.svg.png",
+    path: "https://sourabhsuneja.github.io/academic-resources/class-6/audio/ch-5-excel-working.mp3"
+  },
+  ];
