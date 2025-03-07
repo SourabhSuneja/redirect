443ad071f8ef808de23e009e5ec7046afa1bc062
SourabhSuneja <78584922+SourabhSuneja@users.noreply.github.com>
2025-03-07 11:56:50 +0530

    Create file-list.js

---
+++ test/file-list.js
@@ -0,0 +1,26 @@
+let track_list = [
+  {
+    name: "Chak de India",
+    artist: "Sukhwinder Singh",
+    image: "https://sourabhsuneja.github.io/academic-resources/images/music-icon.png",
+    path: "https://sourabhsuneja.github.io/redirect/test/songs/chak-de-india.mp3"
+  },
+  {
+    name: "Jai ho",
+    artist: "Ravi K Tripathi",
+    image: "https://sourabhsuneja.github.io/academic-resources/images/music-icon.png",
+    path: "https://sourabhsuneja.github.io/redirect/test/songs/jai-ho.mp3"
+  },
+  {
+    name: "Love You Zindagi",
+    artist: "Amit Trivedi",
+    image: "https://sourabhsuneja.github.io/academic-resources/images/music-icon.png",
+    path: "https://sourabhsuneja.github.io/redirect/test/songs/love-you-zindagi.mp3"
+  },
+  {
+    name: "Aashaayein",
+    artist: "K.K.",
+    image: "https://sourabhsuneja.github.io/academic-resources/images/music-icon.png",
+    path: "https://sourabhsuneja.github.io/redirect/test/songs/aashayein.mp3"
+  },
+  ];
